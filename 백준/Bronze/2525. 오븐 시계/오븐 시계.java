13080c995b9ee49141f7080bda11a4edaba4922d
Chanho Park <77423712+taurus429@users.noreply.github.com>
2022-03-09 17:36:45 +0900

    [Bronze IV] Title: 오븐 시계, Time: 84 ms, Memory: 11548 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		int hour = Integer.parseInt(st.nextToken());
+		int minute = Integer.parseInt(st.nextToken());
+		int time = Integer.parseInt(br.readLine());
+		
+		int res = time + minute;
+		hour += res / 60;
+		hour %= 24;
+		minute = res % 60;
+		System.out.println(hour + " " + minute);
+	}
+}
